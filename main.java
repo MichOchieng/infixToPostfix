6c9d412d4b81c7b28779a96eafe2077e9c8f6039
MichOchieng <MichOchieng@gmail.com>
2020-03-23 21:46:22 -0700

    Changes test input

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 public class main {
 
     public static void main(String[] args) {
-        calculator c = new calculator("A+B*(C^D-E)");
+        calculator c = new calculator("(100 + 200) / 2 * 5 + 7");
+        
     }
 
 }
